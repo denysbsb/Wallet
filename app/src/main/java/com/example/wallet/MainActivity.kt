80b79624cdadf26cbd93aa64340d67fced500bc6
denysbsb <denys.design@gmail.com>
2022-04-07 06:44:07 -0300

    remove topbar - databinding - startbalance

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.wallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import com.example.wallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        startBalance()
+    }
+    fun startBalance(){
+        binding.balanceBitcoin.text = "10000"
+        binding.balanceBritas.text = "10000"
     }
 }
